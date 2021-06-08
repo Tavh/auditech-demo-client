efc5489e66c328b504e9ee20458c684dad403c76
Tavh <44731477+Tavh@users.noreply.github.com>
2021-06-08 10:48:06 +0300

    added screenshot

--- src/App.tsx
+++ src/App.tsx
@@ -5,6 +5,7 @@ import PRContainer from './components/prcontainer/prcontainer.component';
 function App() {
   return (
     <div className="App">
+      <img src="https://i.ibb.co/VmX6WmM/pull-requests.png" alt="pull-requests" />
       <PRContainer />
     </div>
   );
